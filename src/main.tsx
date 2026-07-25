7c81dbf4f4e72747e6055f69e69614dc70f9b392
Harish Raj <harishunfounded@gmail.com>
2026-07-25 23:58:22 +0530

    feat: upgrade workspace to React + TypeScript and add DoorEntrance scroll component

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DoorEntrance from './components/DoorEntrance';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <DoorEntrance />
+  </React.StrictMode>
+);
